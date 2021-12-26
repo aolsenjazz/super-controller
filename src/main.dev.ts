9f52d346b145f6fa30395dec7d729751446cc524
Alex Olsen <aolsenjazz@gmail.com>
2021-12-26 15:35:48 -0500

    remove extra log statement

--- src/main.dev.ts
+++ src/main.dev.ts
@@ -109,8 +109,6 @@ const createWindow = async () => {
     shell.openExternal(url);
   });
 
-  console.log('sanity');
-
   // Remove this if your app does not use auto updates
   // eslint-disable-next-line
   new AppUpdater();
