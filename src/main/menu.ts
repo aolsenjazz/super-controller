b34183052cda99d257dc60645ffceed85f14f767
aolsenjazz <aolsenjazz@gmail.com>
2023-04-15 13:13:08 -0400

    remove more unnecessary help menu code

--- src/main/menu.ts
+++ src/main/menu.ts
@@ -306,37 +306,6 @@ export default class MenuBuilder {
                 },
               ],
       },
-      // {
-      //   label: 'Help',
-      //   submenu: [
-      //     {
-      //       label: 'Learn More',
-      //       click() {
-      //         shell.openExternal('https://electronjs.org');
-      //       },
-      //     },
-      //     {
-      //       label: 'Documentation',
-      //       click() {
-      //         shell.openExternal(
-      //           'https://github.com/electron/electron/tree/main/docs#readme'
-      //         );
-      //       },
-      //     },
-      //     {
-      //       label: 'Community Discussions',
-      //       click() {
-      //         shell.openExternal('https://www.electronjs.org/community');
-      //       },
-      //     },
-      //     {
-      //       label: 'Search Issues',
-      //       click() {
-      //         shell.openExternal('https://github.com/electron/electron/issues');
-      //       },
-      //     },
-      //   ],
-      // },
     ];
 
     return templateDefault;
