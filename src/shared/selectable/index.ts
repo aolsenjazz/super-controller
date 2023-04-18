d551015a5f043756d3cd1094baf4d076ff4dc029
aolsenjazz <aolsenjazz@gmail.com>
2023-04-18 19:35:40 -0400

    remove selectable classes

--- src/shared/selectable/index.ts
+++
@@ -1,6 +0,0 @@
-export { FxSelectable } from './fx-selectable';
-export { MultipleValuesSelectable } from './multiple-values-selectable';
-export { Selectable } from './selectable';
-export { BasicSelectable } from './basic-selectable';
-export { UnsetSelectable } from './unset-selectable';
-export { DeviceDriverSelectable } from './device-driver-selectable';
--- src/shared/selectable/selectable.ts
+++
@@ -1,5 +0,0 @@
-export interface Selectable<T> {
-  equals(other: T | string): boolean;
-  get(): T;
-  label(): string;
-}
