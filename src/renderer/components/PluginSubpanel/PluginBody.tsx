5b06ebe5e71dcba315d6419e1dd662dc1c965015
Alexander Olsen <aolsenjazz@gmail.com>
2024-09-08 17:31:50 -0400

    remove old plugin UI rendering

--- src/renderer/components/PluginSubpanel/PluginBody.tsx
+++ src/renderer/components/PluginSubpanel/PluginBody.tsx
@@ -6,15 +6,6 @@ type PropTypes = {
 
 export default function PluginBody(props: PropTypes) {
   const { plugins } = props;
-  // const path = '../../../plugins/input-plugins/basic-override/gui.tsx';
-  const path = '../BasicSelect.tsx';
 
-  // const GUI = require(plugins[0].GUIPath);
-
-  import(`${path}`).then((GUI) => {
-    console.log(GUI);
-  });
-  // const GUI = require('../../../plugins/input-plugins/basic-override/gui.tsx');
-
-  return <div className="plugin-body"></div>;
+  return <div className="plugin-body" />;
 }
