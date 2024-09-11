aa5ecfa1572bbf44f6280c53ea0ef63f03b660e5
Alexander Olsen <aolsenjazz@gmail.com>
2024-09-11 11:03:33 -0400

    docs: plugin manifest

--- src/plugins/plugin-manifest.ts
+++ src/plugins/plugin-manifest.ts
@@ -1,3 +1,8 @@
+/**
+ * Contains meta and descriptive information for all device and input plugins.
+ * Useful for plugin subcomponent discovery, and enumerating all available
+ * plugins.
+ */
 export type PluginManifest = {
   title: string;
   description: string;
