08d4047610eeef0dc671721e73765c485a29a621
Alexander Olsen <aolsenjazz@gmail.com>
2024-09-11 10:56:34 -0400

    docs/refactor: plugins.ts

--- src/renderer/components/PluginSubpanel/plugins.ts
+++ src/renderer/components/PluginSubpanel/plugins.ts
@@ -1,3 +1,6 @@
+/**
+ * Webpack context with which we search for device plugins
+ */
 const deviceContext = require.context(
   '@plugins/device-plugins',
   true,
@@ -5,6 +8,9 @@ const deviceContext = require.context(
   'lazy'
 );
 
+/**
+ * TODO: this will very soon move into the plugins folder
+ */
 type DeviceManifest = {
   gui: string;
   plugin: string;
@@ -13,8 +19,10 @@ type DeviceManifest = {
   description: string;
 };
 
+/**
+ * List of available device plugin manifests, used for subcomponent discovery + import
+ */
 const deviceManifests: DeviceManifest[] = [];
-
 deviceContext.keys().forEach((path) =>
   deviceContext(path).then((m: DeviceManifest) => {
     deviceManifests.push(m);
@@ -22,25 +30,37 @@ deviceContext.keys().forEach((path) =>
   })
 );
 
-export async function importDeviceGUI(pluginTitle: string) {
-  // if for some reason manifests haven't yet been loaded, wait
-  let retries = 0;
+/**
+ * Waits for an array to be non-empty. Useful for making sure that we have discovered all
+ * plugin manifests before we try to access them.
+ */
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+async function waitForArray(arr: any[], interval = 100, retries = 3) {
+  let r = 0;
   await new Promise((resolve, reject) => {
     function checkArr() {
-      if (retries === 3) {
+      if (r === retries) {
         reject(new Error('Unable to load plugin manifests'));
       }
 
-      if (deviceManifests.length > 0) {
+      if (arr.length > 0) {
         resolve(null);
       } else {
-        retries++;
-        setTimeout(checkArr, 100);
+        r++;
+        setTimeout(checkArr, interval);
       }
     }
 
     checkArr();
   });
+}
+
+/**
+ * Dynamically import a device plugin GUI based on the title of the plugin. Throws if unable
+ * to find manifests or the plugin GUI
+ */
+export async function importDeviceGUI(pluginTitle: string) {
+  await waitForArray(deviceManifests);
 
   const manifest = deviceManifests.find((m) => m.title === pluginTitle);
   if (manifest !== undefined) {
