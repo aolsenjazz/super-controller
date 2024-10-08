acb07f6479892ca9a227a7d7de551d843f2c01cf
Alexander Olsen <aolsenjazz@gmail.com>
2024-10-08 10:16:14 -0400

    refactor: clean input plugin creation

--- src/main/menu/input-plugin-menu.ts
+++ src/main/menu/input-plugin-menu.ts
@@ -5,51 +5,37 @@ import {
   getInputManifests,
   importInputSubcomponent,
 } from '@plugins/plugin-loader';
-import { getQualifiedInputId, inputIdFromDriver } from '@shared/util';
 import { MonoInputConfig } from '@shared/hardware-config';
 import { BaseInputPlugin } from '@shared/plugin-core/base-input-plugin';
-import { DRIVERS } from '@shared/drivers';
-import { InteractiveInputDriver } from '@shared/driver-types';
 import { PluginRegistry } from '@main/plugin-registry';
 import { InputRegistry } from '@main/input-registry';
-import { DeviceRegistry } from '@main/device-registry';
 import { PluginManifest } from '@shared/plugin-core/plugin-manifest';
 
 const { MainWindow } = WindowProvider;
 
-export async function createInputPluginMenu(deviceId: string, inputId: string) {
+export async function createInputPluginMenu(qualifiedInputId: string) {
   const manifests = await getInputManifests();
 
   const onClick = async (m: PluginManifest) => {
-    const dev = DeviceRegistry.get(deviceId)!;
-    const devDriver = DRIVERS.get(dev.driverName)!;
-    const input = InputRegistry.get(getQualifiedInputId(deviceId, inputId));
+    const input = InputRegistry.get(qualifiedInputId);
 
-    const inputDriver = devDriver.inputGrids
-      .flatMap((ig) => ig.inputs)
-      .filter((i) => i.interactive === true)
-      .map((i) => i as InteractiveInputDriver)
-      .find((d) => inputIdFromDriver(d) === inputId);
+    if (!(input instanceof MonoInputConfig))
+      throw new Error(`Adding plugin to ${input} is not defined`);
 
-    if (input instanceof MonoInputConfig) {
-      const Plugin = await importInputSubcomponent(m.title, 'plugin');
-      const plugin: BaseInputPlugin = new Plugin(
-        m.title,
-        m.description,
-        inputDriver!
-      );
-      input.plugins.push(plugin.id);
-      PluginRegistry.register(plugin.id, plugin);
+    const inputDriver = input.driver;
+    const Plugin = await importInputSubcomponent(m.title, 'plugin');
+    const plugin: BaseInputPlugin = new Plugin(
+      m.title,
+      m.description,
+      inputDriver
+    );
+    input.plugins.push(plugin.id);
 
-      MainWindow.sendReduxEvent({
-        type: 'inputConfigs/upsertOne',
-        payload: input.toDTO(),
-      });
-    } else {
-      // TODO: How do we handle adding plugin to multi-input configs?
-      // eslint-disable-next-line no-console
-      console.log('Ignoring for now....');
-    }
+    PluginRegistry.register(plugin.id, plugin);
+    MainWindow.sendReduxEvent({
+      type: 'inputConfigs/upsertOne',
+      payload: input.toDTO(),
+    });
   };
 
   return manifests.map((m) => {
