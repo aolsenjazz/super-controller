2f744d68e7e1831c834cb2e1468d99c04ecc7564
Alexander Olsen <aolsenjazz@gmail.com>
2024-10-16 11:47:12 -0400

    xy config panel plugins working

--- src/renderer/components/InputConfigDrawer/InputConfigPanel/XYConfigPanel.tsx
+++ src/renderer/components/InputConfigDrawer/InputConfigPanel/XYConfigPanel.tsx
@@ -1,4 +1,8 @@
+import { selectInputConfigById } from '@features/input-configs/input-configs-slice';
+import { useAppSelector } from '@hooks/use-app-dispatch';
+import { MonoInputDTO } from '@shared/hardware-config/input-config/mono-input-dto';
 import { XYDTO } from '@shared/hardware-config/input-config/xy-config';
+import { getQualifiedInputId } from '@shared/util';
 import { ReactElement } from 'react';
 import OsxTabs from 'renderer/components/OsxTabs';
 import MonoInputConfigPanel from './MonoInputConfigSubpanel';
@@ -11,8 +15,16 @@ export default function XYConfigPanel(props: PropTypes) {
   const { input } = props;
   const { x, y } = input;
 
-  const XConfigPanel = <MonoInputConfigPanel input={x} />;
-  const YConfigPanel = <MonoInputConfigPanel input={y} />;
+  const xConfig = useAppSelector((state) =>
+    selectInputConfigById(state, getQualifiedInputId(input.deviceId, x.id))
+  ) as MonoInputDTO;
+
+  const yConfig = useAppSelector((state) =>
+    selectInputConfigById(state, getQualifiedInputId(input.deviceId, y.id))
+  ) as MonoInputDTO;
+
+  const XConfigPanel = <MonoInputConfigPanel input={xConfig} />;
+  const YConfigPanel = <MonoInputConfigPanel input={yConfig} />;
   const Elements: ReactElement[] = [XConfigPanel, YConfigPanel];
 
   return (
