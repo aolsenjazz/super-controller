cf51d4a5b27878227efe5a1903ca7a51554f64cb
Alexander Olsen <aolsenjazz@gmail.com>
2024-10-18 10:34:41 -0400

    cleanup: refactor backlight-control manifest

--- src/plugins/input-plugins/backlight-control/manifest.ts
+++ src/plugins/input-plugins/backlight-control/manifest.ts
@@ -4,21 +4,21 @@ import { MonoInputConfig } from '@shared/hardware-config/input-config/mono-input
 
 import { InputPluginManifest } from '@shared/plugin-core/input-plugin-manifest';
 
+function colorCapableRequirement(config: BaseInputConfig): boolean {
+  if (config instanceof MonoInputConfig && config.driver.interactive) {
+    const asMonoInteractive = config.driver as MonoInteractiveDriver;
+    return asMonoInteractive.availableColors.length > 0;
+  }
+
+  return false;
+}
+
 const Manifest: InputPluginManifest = {
   title: 'Backlight Control',
   description: 'Controls the color of the lights for this input.',
   gui: 'backlight-control/gui/gui.tsx',
   plugin: 'backlight-control/index.ts',
-  requirements: [
-    (config: BaseInputConfig) => {
-      if (config instanceof MonoInputConfig && config.driver.interactive) {
-        const asMonoInteractive = config.driver as MonoInteractiveDriver;
-        return asMonoInteractive.availableColors.length > 0;
-      }
-
-      return false;
-    },
-  ],
+  requirements: [colorCapableRequirement],
 };
 
 export default Manifest;
