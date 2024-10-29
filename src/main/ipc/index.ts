b965a8949eba2c29e66ce5f4109f59f78d8c3f82
Alexander Olsen <aolsenjazz@gmail.com>
2024-10-29 10:44:03 -0400

    fix main ipc event types

--- src/main/ipc/index.ts
+++ src/main/ipc/index.ts
@@ -1,5 +1,5 @@
 import os from 'os';
-import { ipcMain, Event, shell } from 'electron';
+import { ipcMain, shell, IpcMainEvent } from 'electron';
 
 import { controllerRequest, fivePinRequest } from '@shared/email-templates';
 
@@ -12,12 +12,12 @@ import './initialize-device-config-ipc';
 import './initialize-input-config-ipc';
 
 // When the frontend as for OS details, send them
-ipcMain.on(HOST.OS, (e: Event) => {
+ipcMain.on(HOST.OS, (e: IpcMainEvent) => {
   e.returnValue = os.platform();
 });
 
 // User wants to send an email
-ipcMain.on(HOST.REQUEST, (_e: Event, deviceName: string) => {
+ipcMain.on(HOST.REQUEST, (_e: IpcMainEvent, deviceName: string) => {
   const template = deviceName
     ? controllerRequest(deviceName)
     : fivePinRequest();
@@ -26,18 +26,18 @@ ipcMain.on(HOST.REQUEST, (_e: Event, deviceName: string) => {
   );
 });
 
-ipcMain.on(LAYOUT.GET_LAYOUT, (e: Event) => {
+ipcMain.on(LAYOUT.GET_LAYOUT, (e: IpcMainEvent) => {
   e.returnValue = Store.getLayoutParams();
 });
 
-ipcMain.on(LAYOUT.SET_LAYOUT, (e: Event, lp: LayoutParams) => {
+ipcMain.on(LAYOUT.SET_LAYOUT, (e: IpcMainEvent, lp: LayoutParams) => {
   e.returnValue = Store.setLayoutParams(lp);
 });
 
-ipcMain.on(LAYOUT.GET_LAYOUT_ITEM, (e: Event, s: string) => {
+ipcMain.on(LAYOUT.GET_LAYOUT_ITEM, (e: IpcMainEvent, s: string) => {
   e.returnValue = Store.getLayoutItem(s);
 });
 
-ipcMain.on(LAYOUT.SET_LAYOUT_ITEM, (e: Event, s: string, v: string) => {
+ipcMain.on(LAYOUT.SET_LAYOUT_ITEM, (e: IpcMainEvent, s: string, v: string) => {
   e.returnValue = Store.setLayoutItem(s, v);
 });
